3206995d0460b607be60ad7e07238d585755a067
Kent Butler <KentDavidButler@gmail.com>
2021-10-30 13:33:08 -0400

    Allow Listing IP Addresses via Discord Bot

---
+++ Azure_Updates.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import os
+from azure.cli.core import get_default_cli
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+SUB_ID = os.getenv('AZURE_SUBSCRIPTION_ID')
+TEN_ID = os.getenv('AZURE_TENANT_ID')
+CLI_ID = os.getenv('AZURE_CLIENT_ID')
+PASSWORD = os.getenv('AZURE_CLIENT_SECRET')
+
+RESOURCE_GROUP = os.getenv('RESOURCE_GROUP')
+SERVER_NAME = os.getenv('SERVER_NAME')
+SERVER_NSG_NAME = os.getenv('SERVER_NSG_NAME')
+NSG_RULE = os.getenv('NSG_RULE')
+
+
+def az_cli(args_str):
+    args = args_str.split()
+    cli = get_default_cli()
+    cli.invoke(args)
+    if cli.result.result:
+        response = cli.result.result
+        return response
+    elif cli.result.error:
+        err_response = cli.result.error
+        raise err_response
+    return True
+
+
+def az_login():
+    login_string = f"login --service-principal -u {CLI_ID} -p {PASSWORD} --tenant {TEN_ID}"
+    response = az_cli(login_string)
+
+    return response
+
+
+def az_get_nsg():
+    cli_command = f"network nsg rule show --resource-group {RESOURCE_GROUP}" \
+                  f" --nsg-name {SERVER_NSG_NAME} -n {NSG_RULE}"
+    response = az_cli(cli_command)
+    current_ip_addresses = response["sourceAddressPrefixes"]
+
+    return current_ip_addresses
+
+
+def az_set_nsg_rules(ip_list):
+    try:
+        print(f"****{ip_list} is not a string {type(ip_list)}")
+        cli_command = f"network nsg rule update --resource-group {RESOURCE_GROUP} " \
+                      f"--nsg-name {SERVER_NSG_NAME} -n {NSG_RULE} --source-address-prefixes {' '.join(ip_list)}"
+        az_cli(cli_command)
+        return
+    except Exception as x:
+        return f"Azure Exception: {x}"
--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python3
+
 # bot.py
 import os
 import csv
-import random
 import socket
+import ipaddress
+
+import Azure_Updates as az
 
 import discord
 from discord.ext import commands
@@ -17,63 +21,85 @@ bot = commands.Bot(command_prefix='!', case_insensitive=True)
 wish_file = os.path.join(os.environ['USERPROFILE'], "Documents", "wishes.csv")
 ip_file = os.path.join(os.environ['USERPROFILE'], "Documents", "ipaddresses.csv")
 
+
 @bot.event
 async def on_ready():
+    az.az_login()
     print("Everything's all ready to go~")
+
 
 @bot.command(name='AddToWishList')
 async def AddToWishList(ctx, *args):
-    '''
+    """
     :: Adds an string of text to the wish list.
-    '''
+    """
     wish = ' '.join(args)
 
-    success = append_list_as_row(wish_file,("Wish::"+wish))
-    if (success==0):
+    success = append_list_as_row(wish_file, ("Wish::"+wish))
+    if success == 0:
         await ctx.channel.send(f"The following was added to wish list: {wish}")
     else:
-        await ctx.channel.send(f"An Error occured adding to wish list")
+        await ctx.channel.send(f"An Error occurred adding to wish list")
+
 
 @bot.command(name='ShowWishList')
 async def ShowWishList(ctx):
-    '''
+    """
     :: View items in the wish list
-    '''
+    """
     wish_list = return_csv_as_list(wish_file)
     for wishes in wish_list:
         await ctx.channel.send(wishes)
 
     await ctx.channel.send(f"End of list")
 
+
 @bot.command(name='AddIPAddress')
 async def AddIPAddress(ctx, *args):
-    '''
-    :: Only allows valid IPv4 Addresses to join the Server
-    '''
+    """
+    :: Only allows valid IPv4 Addresses to join the Terraria Server
+    """
 
     channel = ctx.channel.id
-    ipAddress = ' '.join(args)
+    ipAddress = ''.join(args)
 
     if channel == int(seceretChannel):
         try:
             # check if the IP address is valid
-            socket.inet_aton(ipAddress)
+            ipaddress.ip_address(str(ipAddress))
 
-            success = append_list_as_row(ip_file, (ipAddress))
-            if (success==0):
+            # check if IP is already in list
+            ip_addresses = return_csv_as_list(ip_file)
+            if ipAddress in ip_addresses:
+                await ctx.channel.send(f"The following IP already exists in IP Address list: {ipAddress}")
+                return
+
+            success = append_list_as_row(ip_file, ipAddress)
+            if success == 0:
                 await ctx.channel.send(f"The following was added to IP Address list: {ipAddress}")
             else:
-                await ctx.channel.send(f"An Error occured adding to IP Address list")
-        except:
-            await ctx.channel.send('Invalid IP Address try using https://whatismyipaddress.com')
+                await ctx.channel.send(f"An Error occurred adding to IP Address list")
+
+            # add the new IP addresses to to the list of IPs pulled from the CSV file
+            ip_addresses.append(ipAddress)
+
+            try:
+                print(az.az_set_nsg_rules(ip_addresses))
+            except Exception as z:
+                await ctx.channel.send(f'Exception trying to add ipAddress to Azure:{z}')
+
+        except Exception as x:
+            await ctx.channel.send(f'Exception:{x}')
+            await ctx.channel.send(f'Invalid IP Address {ipAddress}. Look up IP here https://whatismyipaddress.com')
     else:
         await ctx.channel.send('Cannot use command here')
 
+
 @bot.command(name='ShowIPAddresses')
 async def ShowIPAddresses(ctx):
-    '''
+    """
     :: View list of added IP Addresses
-    '''
+    """
     channel = ctx.channel.id
 
     if channel == int(seceretChannel):
@@ -85,11 +111,12 @@ async def ShowIPAddresses(ctx):
     else:
         await ctx.channel.send('Cannot use command here')
 
+
 @bot.command(name='ping')
 async def ping(ctx):
-    '''
+    """
     :: checks the ping of the bot.
-    '''
+    """
 
     # Get the latency of the bot
     latency = bot.latency  # Included in the Discord.py library
@@ -99,9 +126,9 @@ async def ping(ctx):
 
 @bot.command(name='echo')
 async def echo(ctx, *args):
-    '''
+    """
     :: repeats any said after !echo.
-    '''
+    """
     response = ""
     for arg in args:
         response = response + " " + arg
@@ -121,7 +148,7 @@ def append_list_as_row(file_name, list_of_elem):
         write_obj.close()
         return 0
     except:
-        #unknown error handled
+        # unknown error handled
         return 1
 
 
@@ -131,20 +158,21 @@ def return_csv_as_list(file_name):
     line_count = 0
     with open(file_name, 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
-        output_list.append(next(csv_reader))
+        # output_list.append(next(csv_reader))
 
         for row in csv_reader:
-            output_list.append(f'\t{row}')
+            output_list.append(''.join(row))
     csv_file.close()
     return output_list
+
 
 @AddToWishList.error
 async def AddToWishList_error(ctx, error):
     if isinstance(error, commands.BadArgument):
-        await ctx.send('A Bad Argument error has occured')
+        await ctx.send('A Bad Argument error has occurred')
     elif isinstance(error, commands.CommandInvokeError):
-        await ctx.send('A Command Invoke Error occured')
+        await ctx.send('A Command Invoke Error occurred')
     else:
-        await ctx.send('Something really wrong occured')
+        await ctx.send('Something really wrong occurred')
 
 bot.run(TOKEN)
